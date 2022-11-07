5054f2239fdd451743f080894214f1b164ce48ad
Sean Lim <seanlimdev856@gmail.com>
2022-11-07 15:24:49 +0800

    fix base path in nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,10 +1,11 @@
 <script>
+	import { base } from '$app/paths';
 	import { PUBLIC_HOSTNAME } from '$env/static/public';
 </script>
 
 <nav class="navbar p-2 px-3" aria-label="main navigation">
 	<div class="navbar-brand">
-		<a href="/">
+		<a href={`${base}/`}>
 			<div class="navbar-item logo-button">
 				<img class="logo mr-2" src={`${PUBLIC_HOSTNAME}/logo.webp`} alt="logo" />
 				<h3 class="title is-4">NUS USC Wall</h3>
