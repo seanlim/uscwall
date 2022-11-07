484d5e2d3ba1659b6eef0de4b6f08c141eab54b0
Sean Lim <seanlimdev856@gmail.com>
2022-11-07 14:56:16 +0800

    use base instead

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
 	import Nav from '../components/Nav.svelte';
+	import { PUBLIC_HOSTNAME } from '$env/static/public';
 </script>
+
+<svelte:head>
+	<base href={PUBLIC_HOSTNAME} />
+</svelte:head>
 
 <Nav />
 <div class="container is-flex is-flex-direction-column">
