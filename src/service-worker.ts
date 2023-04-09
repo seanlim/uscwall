e252ca37ce886289dd0a2019b090a66fa523685c
Sean Lim <seanlimdev856@gmail.com>
2023-04-09 08:46:06 +0800

    cache more aggressively

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -15,45 +15,43 @@ self.addEventListener('fetch', (event) => {
 	// skip requests for extensions
 	if (!(event.request.url.indexOf('http') === 0)) return;
 
-	if (event.request.url.includes('routes')) {
-		event.respondWith(
-			caches.match(event.request).then((response) => {
-				if (isValid(response)) {
-					// Cache hit - return response
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			if (isValid(response)) {
+				// Cache hit - return response
+				return response;
+			}
+
+			return fetch(event.request).then((response) => {
+				// Check if we received a valid response
+				if (!response || response.status !== 200 || response.type !== 'basic') {
 					return response;
 				}
 
-				return fetch(event.request).then((response) => {
-					// Check if we received a valid response
-					if (!response || response.status !== 200 || response.type !== 'basic') {
-						return response;
-					}
+				// IMPORTANT: Clone the response. A response is a stream
+				// and because we want the browser to consume the response
+				// as well as the cache consuming the response, we need
+				// to clone it so we have two streams.
+				const responseToCache = response.clone();
+				const headers = new Headers(responseToCache.headers);
+				headers.append('sw-fetched-on', new Date().getTime().toString());
 
-					// IMPORTANT: Clone the response. A response is a stream
-					// and because we want the browser to consume the response
-					// as well as the cache consuming the response, we need
-					// to clone it so we have two streams.
-					const responseToCache = response.clone();
-					const headers = new Headers(responseToCache.headers);
-					headers.append('sw-fetched-on', new Date().getTime().toString());
-
-					caches.open(CACHE_NAME).then((cache) => {
-						responseToCache.blob().then((body) => {
-							cache.put(
-								event.request,
-								new Response(body, {
-									status: responseToCache.status,
-									statusText: responseToCache.statusText,
-									headers
-								})
-							);
-						});
+				caches.open(CACHE_NAME).then((cache) => {
+					responseToCache.blob().then((body) => {
+						cache.put(
+							event.request,
+							new Response(body, {
+								status: responseToCache.status,
+								statusText: responseToCache.statusText,
+								headers
+							})
+						);
 					});
-					return response;
 				});
-			})
-		);
-	}
+				return response;
+			});
+		})
+	);
 });
 
 export {};
